62efcb5059a194a95b076cfbe14630378387de5f
Luiz Carvalho <luizrogeriocarvalho@gmail.com>
2019-04-11 18:31:33 -0300

    Still improving Docker container deployment.

---
+++ backup.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+PLAYDIR="`dirname $0`"
+
+cd "$PLAYDIR"
+
+BASEDIR="`pwd`"
+CONFDIR="${BASEDIR}/conf.d"
+SSHCONF="${CONFDIR}/ssh_config"
+
+# Ensure priv keys in conf.d dir are protected
+chmod go-w,o-rwx ${CONFDIR}/*
+
+export BASEDIR CONFDIR SSHCONF
+
+export DISPLAY_SKIPPED_HOSTS="false"
+
+export ANSIBLE_SSH_ARGS="-C -o ControlMaster=auto -o ControlPersist=60s -F ${SSHCONF}"
+
+export BKPTAG="`date +%Y%m%d%H`"
+#export BKPTAG="201901222049"
+
+# Useful tags to skip
+# From common: (these first two must be called when python is missing)
+#  - install_dep_pkg
+#  - bootstrap_python
+#  - update_repository
+
+ansible-playbook -i hosts --extra-vars "gather_y_n=false basedir=${BASEDIR} confdir=${CONFDIR} sshconf=${SSHCONF} bkptag=${BKPTAG}" Backup.yml 
+
+rm -f *.retry
