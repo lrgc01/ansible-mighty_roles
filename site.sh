b2dfbf6c4e390ea922eb712707d8cee1b30cf774
Luiz Carvalho <luizrogeriocarvalho@gmail.com>
2019-04-21 16:11:06 -0300

    Added general config "site" using webservers.yml group_vars. Fixes templates.

--- site.sh
+++ site.sh
@@ -75,13 +75,14 @@ export ANSIBLE_SSH_ARGS="-C -o ControlMaster=auto -o ControlPersist=60s -F ${SSH
 # update_cache_y_n=yes or no
 
 # First, full, playbook running - no skip
-SKIP_TAGS=""
+#SKIP_TAGS=""
 # After first install, the tag "bootstrap_python" can be safely skipped:
-#SKIP_TAGS="--skip-tags \"bootstrap_python\""
+SKIP_TAGS="--skip-tags bootstrap_python"
 
-ansible-playbook -i hasrv --extra-vars "gather_y_n=false update_cache_y_n=no basedir=${BASEDIR} confdir=${CONFDIR} sshconf=${SSHCONF}" $SKIP_TAGS HAservers.yml 
-ansible-playbook -i dbsrv --extra-vars "gather_y_n=false update_cache_y_n=no basedir=${BASEDIR} confdir=${CONFDIR} sshconf=${SSHCONF}" $SKIP_TAGS DBservers.yml 
-ansible-playbook -i dockersrv --extra-vars "gather_y_n=yes update_cache_y_n=yes basedir=${BASEDIR} confdir=${CONFDIR} sshconf=${SSHCONF}" $SKIP_TAGS Dockers.yml 
+#ansible-playbook -i hasrv --extra-vars "gather_y_n=false update_cache_y_n=no basedir=${BASEDIR} confdir=${CONFDIR} sshconf=${SSHCONF}" $SKIP_TAGS HAservers.yml 
+#ansible-playbook -i dbsrv --extra-vars "gather_y_n=false update_cache_y_n=no basedir=${BASEDIR} confdir=${CONFDIR} sshconf=${SSHCONF}" $SKIP_TAGS DBservers.yml 
+#ansible-playbook -i dockersrv --extra-vars "gather_y_n=yes update_cache_y_n=yes basedir=${BASEDIR} confdir=${CONFDIR} sshconf=${SSHCONF}" $SKIP_TAGS Dockers.yml 
+ansible-playbook -i websrv --extra-vars "gather_y_n=no update_cache_y_n=no basedir=${BASEDIR} confdir=${CONFDIR} sshconf=${SSHCONF}" $SKIP_TAGS Site.yml 
 
 # garbage
 rm -f *.retry
